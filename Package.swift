8cd6d6ff7ef3eb6b4a35f1a7e231c5c577f6eb09
Jaehong Kang <sinoru@me.com>
2023-02-26 16:17:03 +0900

    Adds URLEncodedFormCoder module

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "URLEncodedFormCoder",
-            targets: ["URLEncodedFormEncoder"]),
+            targets: ["URLEncodedFormCoder"]),
         .library(
             name: "URLEncodedFormEncoder",
             targets: ["URLEncodedFormEncoder"])
@@ -29,6 +29,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "URLEncodedFormCoder",
+            dependencies: ["URLEncodedFormEncoder"]),
         .target(
             name: "URLEncodedFormEncoder",
             dependencies: [
---
+++ Sources/URLEncodedFormCoder/URLEncodedFormCoder.swift
@@ -0,0 +1,8 @@
+//
+//  URLEncodedFormCoder.swift
+//  
+//
+//  Created by Jaehong Kang on 2023/02/26.
+//
+
+@_exported import URLEncodedFormEncoder
